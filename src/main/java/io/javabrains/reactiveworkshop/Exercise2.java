eed9b5f94db6c8118525192d4c2aca6d4e83b2f2
NickEU <k@nick.tf>
2022-12-26 22:31:00 +0200

    ex 2 simple sub to flux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 public class Exercise2 {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
-        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
-
-        // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
-
-        // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
