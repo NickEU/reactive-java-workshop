a1e984e0dda47b514210488f3aa2365b06ada7f0
NickEU <k@nick.tf>
2022-12-26 23:48:10 +0200

    ex 3-4 simple blocking & non-blocking with mono & flux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,20 +1,37 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Get all numbers in the ReactiveSources.intNumbersFlux stream " +
+                "into a List and print the list and its size");
 
-        // Use ReactiveSources.intNumbersFlux()
-
-        // Get all numbers in the ReactiveSources.intNumbersFlux stream
-        // into a List and print the list and its size
-        // TODO: Write code here
-
+        getListWithBlocking();
+        
+        getListNonBlocking();
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+    private static void getListNonBlocking() {
+        System.out.println("Getting list without blocking: ");
+
+        Mono<List<Integer>> listMono = ReactiveSources.intNumbersFlux().collectList();
+        listMono.subscribe(list -> System.out.println(list + "\nSize of list: " + list.size()));
+    }
+
+    private static void getListWithBlocking() {
+        System.out.println("Getting list with blocking: ");
+
+        var numbers = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println(numbers);
+        System.out.println("Size of list: " + numbers.size());
     }
 
 }
--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,18 +1,16 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class Exercise4 {
 
     public static void main(String[] args) throws IOException {
-
-        // Use ReactiveSources.intNumberMono()
-
-        // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        System.out.println("Print the value from intNumberMono when it emits");
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        Integer num = ReactiveSources.intNumberMono().block(Duration.ofSeconds(5));
 
         System.out.println("Press a key to end");
         System.in.read();
